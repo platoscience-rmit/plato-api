0d0cb00324335d27398ba86000149f647138e556
THuy Chi <chi.hathuy15@gmail.com>
2025-06-27 21:30:52 +0700

    create account

--- apps/users/repositories/account_repository.py
+++ apps/users/repositories/account_repository.py
@@ -4,3 +4,10 @@ from apps.common.base_repository import BaseRepository
 class AccountRepository(BaseRepository):
     def __init__(self):
         super().__init__(Account)
+
+    def create_account_repo(self, email: str, password: str, user_id) -> Account:
+        return self.create(
+            email=email, 
+            password=password, 
+            user_id=user_id
+        )
--- apps/users/services/account_service.py
+++ apps/users/services/account_service.py
@@ -4,5 +4,13 @@ from apps.users.repositories.account_repository import AccountRepository
 class AccountService(BaseService):
     def __init__(self):
         super().__init__(AccountRepository())
+        self.account_repo = AccountRepository()
+
+    def create_account_service(self, email: str, password: str, user_id):
+        return self.account_repo.create_account_repo(
+            email=email,
+            password=password,
+            user_id=user_id
+        )
 
    
