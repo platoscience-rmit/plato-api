73f60cf42d516e390c3e9b49e7409e67bb278b00
VinhD1nk <vinhduaws@gmail.com>
2025-06-30 19:43:34 +0700

    update render deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn config.wsgi:application --env DJANGO_SETTINGS_MODULE=config.settings.staging
+web: python manage.py migrate
--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=impuawayvfr1e*&_9i^=31e(c)x$+huuxpriqs@_uy1wd=n+g'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
