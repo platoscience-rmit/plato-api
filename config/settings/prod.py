8944677823c9c72d018f7182fe6a9133d40fa62b
khue <khue251103@gmail.com>
2025-07-25 18:39:09 +0700

    add prod settings

---
+++ config/settings/prod.py
@@ -0,0 +1,15 @@
+from .base import *
+import dj_database_url
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+ALLOWED_HOSTS = ['plato-api-prod.onrender.com']
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
+DEBUG = False
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
