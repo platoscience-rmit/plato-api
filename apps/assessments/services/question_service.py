a77f22c56dcf062893a7f3d6ed79dd0350fd31a2
THuy Chi <chi.hathuy15@gmail.com>
2025-08-19 18:05:22 +0700

    fix: handle missing selected option case

--- apps/assessments/services/question_service.py
+++ apps/assessments/services/question_service.py
@@ -12,12 +12,15 @@ class QuestionService(BaseService):
             for answer in  answers_data:
                 matching_question = self.filter(category=requested_category, id=answer.get("question").id)
                 if matching_question:
+                    print(matching_question)
                     if requested_category=="text" or requested_category=="analytic":
                         questions.append({
                             "question": matching_question[0],
                             "answer": answer.get("answer")
                         })
                     else:
+                        if answer.get("selected_option") is None:
+                            raise Exception(f"{matching_question[0]} missing selected option.")
                         questions.append({
                             "question": matching_question[0],
                             "selected_option": answer.get("selected_option").id
