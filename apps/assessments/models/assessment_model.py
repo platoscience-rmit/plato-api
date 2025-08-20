88fdcd2673e6d762a33d9f8d6459f289419fcd86
khue <khue251103@gmail.com>
2025-08-21 00:31:56 +0700

    fix: checkin_date, protocol_selected_date field to datetime

--- apps/assessments/models/assessment_model.py
+++ apps/assessments/models/assessment_model.py
@@ -29,7 +29,7 @@ class Assessment(models.Model):
         null=True, 
         blank=True
     )
-    protocol_selected_date = models.DateField(null=True, blank=True)
+    protocol_selected_date = models.DateTimeField(null=True, blank=True)
     stopped_date = models.DateTimeField(null=True, blank=True)
     stop_reason = models.TextField(null=True, blank=True)
     severity = models.IntegerField(choices=SEVERITY_CHOICES, null=True, blank=True)
